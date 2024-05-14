c4a83903f59c0c8f20c0f9ecf1379db7c94aa1b0
WIP-maesch <119851162+WIP-maesch@users.noreply.github.com>
2024-05-14 10:17:20 +0200

    Unnötige Files gegen nötige ausgetauscht
    Bewertung nach N. Schneeberger

--- script.js
+++ script.js
@@ -34,3 +34,107 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   })
 })
+
+
+/*const myChart = new Chart(ctx, { ... });*/
+
+
+import Chart from 'chart.js/auto';
+
+
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Fribourg', 'Bern', 'Zurich', 'Chur'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+
+
+  /* JS von Nick*/
+  console.log("Hello, World!");
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://376009-28.web.fhgr.ch/endpoint.php');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function main() {
+    let data = await fetchData();
+    console.log(data);
+
+    let date = data.data.date;
+    let temp_chur = data.data.chur;
+    let temp_bern = data.data.bern;
+
+    const ctx = document.getElementById('temperatureChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: date,
+            datasets: [{
+                label: 'Temperature in Chur',
+                data: temp_chur,
+                backgroundColor: 'red',
+                borderColor: 'red',
+                borderWidth: 1
+            },
+            {
+                label: 'Temperature in Bern',
+                data: temp_bern,
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        displayFormats: {
+                            day: 'DD.MM HH:M'
+                        }
+                    },
+                    ticks: {
+                        source: 'labels',
+                        maxTicksLimit: 10,
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Temperature (°C)'
+                    }
+                }
+            }
+        }
+    });
+
+}
+
+main();
+
